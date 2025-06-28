cf43c199ba06a9ee2a070d6b0da098279f40d12c
0xKANARIA <enashinonome264@gmail.com>
2025-06-28 09:41:20 +0000

    Android-CI: tmate-ssh: move tmate dependency to common
    [KanariaAlt] no need to write it directly in the workflow just run dependency.sh for all workflows
    
    Signed-off-by: mizuenaAlt <enashinonome264@gmail.com>

--- Common/Dependency.sh
+++ Common/Dependency.sh
@@ -4,4 +4,8 @@ sudo apt-get update
 sudo apt-get install -y build-essential clang bc bison libssl-dev libfl-dev libncurses5-dev libzstd-dev libtinfo5
 sudo apt-get install -y curl git ftp lftp wget libarchive-tools ccache python2 python2-dev python3
 sudo apt-get install -y zip unzip tar gzip bzip2 rar unrar cpio
+sudo apt-get install tmate 
+sudo apt-get purge needrestart 
+sudo apt install openssh-server 
+sudo systemctl enable --now ssh
           
