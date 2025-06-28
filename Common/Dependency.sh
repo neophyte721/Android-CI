2f41b9f3e0fb09fd90b83c4d3b2cb83cc6aa87ea
0xKANARIA <enashinonome264@gmail.com>
2025-06-28 14:10:12 +0000

    Android-CI: Add free disk space and add licenses
    [Kanaria] clean up some useless stuff
    
    Signed-off-by: mizuenaAlt <enashinonome264@gmail.com>

--- Common/Dependency.sh
+++ Common/Dependency.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "=============================================================================="
+echo "Update More Package & Install Dependecy"
+echo "=============================================================================="
 
 sudo apt-get update
 sudo apt-get install -y build-essential clang bc bison libssl-dev libfl-dev libncurses5-dev libzstd-dev libtinfo5 gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib
