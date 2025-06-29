b69458b2ec758d5c51baebc4dc513c004c31ef70
0xKANARIA <enashinonome264@gmail.com>
2025-06-29 23:50:29 +0700

    Android-CI: build-clang: drop bolt flags

--- Common/clang.sh
+++ Common/clang.sh
@@ -34,7 +34,6 @@ chmod +x build-llvm.py
     --defines LLVM_PARALLEL_COMPILE_JOBS="$(nproc)" LLVM_PARALLEL_LINK_JOBS="$(nproc)" CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
     --install-folder "$install" \
     --quiet-cmake \
-    --bolt \
     --shallow-clone \
     --targets ARM AArch64 X86 \
     --ref "release/18.x" \
