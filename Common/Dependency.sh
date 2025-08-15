d6de9dbe13827616f36f4124c5479d49c882ef36
neophyte <alprjkt@gmail.com>
2025-08-15 19:13:13 +0700

    CI: fix typo:)

--- Common/Dependency.sh
+++ Common/Dependency.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y build-essential clang bc bison libssl-dev libfl-dev libn
 sudo apt-get install -y curl git ftp lftp wget libarchive-tools ccache python2 python2-dev python3 libc6-dev lib32ncurses-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils
 sudo apt-get install -y zip unzip tar gzip bzip2 rar unrar cpio xsltproc bc lib32readline-dev lib32z1-dev liblz4-tool libncurses-dev libsdl1.2-dev build-essential libgtk-3-dev libglu1-mesa-dev freeglut3-dev libxml2 lzop pngcrush schedtool squashfs-tools
 sudo apt-get install -y tmate imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libncurses6 python3 tar
-sudo apt-get install -y cache
+sudo apt-get install -y ccache
 sudo apt-get purge needrestart 
 sudo apt install openssh-server 
 sudo systemctl enable --now ssh
